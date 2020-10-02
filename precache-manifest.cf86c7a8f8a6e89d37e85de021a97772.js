89ace2dc129d0f5cebf9d7e939d7571b9d51e550
SarkadiLaszlo <39276824+SarkadiLaszlo@users.noreply.github.com>
2020-10-02 13:14:53 +0200

    Updates

---
+++ precache-manifest.cf86c7a8f8a6e89d37e85de021a97772.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b4bb30f0749019a20d6d9f01c3ee2be",
+    "url": "/nemetrehangolva/index.html"
+  },
+  {
+    "revision": "0f1266383d96423b1c8a",
+    "url": "/nemetrehangolva/static/css/main.298f8ba4.chunk.css"
+  },
+  {
+    "revision": "e9a6be2595b4e92c1174",
+    "url": "/nemetrehangolva/static/js/2.efaa8b28.chunk.js"
+  },
+  {
+    "revision": "664cfc81cef114fb35ad4cf2853781b4",
+    "url": "/nemetrehangolva/static/js/2.efaa8b28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f1266383d96423b1c8a",
+    "url": "/nemetrehangolva/static/js/main.d52b73c6.chunk.js"
+  },
+  {
+    "revision": "8e9c1bf4dbb955119e43",
+    "url": "/nemetrehangolva/static/js/runtime-main.2e926c01.js"
+  },
+  {
+    "revision": "1f00f42fead8289bb8a4761382b259cc",
+    "url": "/nemetrehangolva/static/media/alap1.1f00f42f.jpg"
+  },
+  {
+    "revision": "27e2a62d2b5d2f683cd782f30c725386",
+    "url": "/nemetrehangolva/static/media/alap11.27e2a62d.jpg"
+  },
+  {
+    "revision": "4b35fcc6ec21b9e24bc8cc1a8dacc32f",
+    "url": "/nemetrehangolva/static/media/alap2.4b35fcc6.jpg"
+  },
+  {
+    "revision": "c030deba95627275caa618f001d4c003",
+    "url": "/nemetrehangolva/static/media/alap21.c030deba.jpg"
+  },
+  {
+    "revision": "1f2fffb7163c3ef6ed7cd677acb22397",
+    "url": "/nemetrehangolva/static/media/alap3.1f2fffb7.jpg"
+  },
+  {
+    "revision": "d16aad238d511ffa58c22741770c8326",
+    "url": "/nemetrehangolva/static/media/alap31.d16aad23.jpg"
+  },
+  {
+    "revision": "17c56bdbf612d45d2708829a6dbb73cf",
+    "url": "/nemetrehangolva/static/media/alap4.17c56bdb.jpg"
+  },
+  {
+    "revision": "c8e5df4144b179dc7abc12e336ef402f",
+    "url": "/nemetrehangolva/static/media/alap41.c8e5df41.jpg"
+  },
+  {
+    "revision": "ddbf81950ed001fc5a1e65edbf3c873e",
+    "url": "/nemetrehangolva/static/media/alap5.ddbf8195.jpg"
+  },
+  {
+    "revision": "284deab6746238b80ea416ac90bde896",
+    "url": "/nemetrehangolva/static/media/alap51.284deab6.jpg"
+  },
+  {
+    "revision": "85aae5d9ccae5bc4d1099750ef319fe9",
+    "url": "/nemetrehangolva/static/media/alap6.85aae5d9.jpg"
+  },
+  {
+    "revision": "6fed27d02f12a9dd00c02de99b74ae8d",
+    "url": "/nemetrehangolva/static/media/alap61.6fed27d0.jpg"
+  },
+  {
+    "revision": "edf0c07eeb349eed943905a954fd5a99",
+    "url": "/nemetrehangolva/static/media/alap7.edf0c07e.jpg"
+  },
+  {
+    "revision": "c6197007d27c2500adf55689206deaa5",
+    "url": "/nemetrehangolva/static/media/alap71.c6197007.jpg"
+  },
+  {
+    "revision": "d1b9c21950622125de2f350b5fae6f1e",
+    "url": "/nemetrehangolva/static/media/alap8.d1b9c219.jpg"
+  },
+  {
+    "revision": "2acec85bd469fcb06f8bc85966a1bab5",
+    "url": "/nemetrehangolva/static/media/buttonBelepes.2acec85b.png"
+  },
+  {
+    "revision": "d250d03183ea432c678b9cd3cbea319c",
+    "url": "/nemetrehangolva/static/media/buttonFeladatok.d250d031.png"
+  },
+  {
+    "revision": "344fd9a5753cf366b506fc524a01cf46",
+    "url": "/nemetrehangolva/static/media/buttonOktatas.344fd9a5.png"
+  },
+  {
+    "revision": "c6d0fd8b027133b90afdfafb27043e55",
+    "url": "/nemetrehangolva/static/media/buttonRolam.c6d0fd8b.png"
+  },
+  {
+    "revision": "9e3c8b7215832a7c47913a6b42b76070",
+    "url": "/nemetrehangolva/static/media/buttonVizsga.9e3c8b72.png"
+  }
+]);
