0b30ae13fdddaffa2e9e662b1e95e483b42b9d77
SarkadiLaszlo <39276824+SarkadiLaszlo@users.noreply.github.com>
2020-10-18 16:09:13 +0200

    +

---
+++ build/precache-manifest.64ac66c4de7c1b5117c767549ef80257.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9690cbc7a6510eeb0cf8eb27281a95b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8fb3937c8ed73f6fad1c",
+    "url": "/static/css/main.f92b7f56.chunk.css"
+  },
+  {
+    "revision": "f1fe664cff932285e6e3",
+    "url": "/static/js/2.fd22c6d3.chunk.js"
+  },
+  {
+    "revision": "664cfc81cef114fb35ad4cf2853781b4",
+    "url": "/static/js/2.fd22c6d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fb3937c8ed73f6fad1c",
+    "url": "/static/js/main.7f81bb3d.chunk.js"
+  },
+  {
+    "revision": "005e92f897580fad41ae",
+    "url": "/static/js/runtime-main.4456a21c.js"
+  },
+  {
+    "revision": "1f00f42fead8289bb8a4761382b259cc",
+    "url": "/static/media/alap1.1f00f42f.jpg"
+  },
+  {
+    "revision": "27e2a62d2b5d2f683cd782f30c725386",
+    "url": "/static/media/alap11.27e2a62d.jpg"
+  },
+  {
+    "revision": "4b35fcc6ec21b9e24bc8cc1a8dacc32f",
+    "url": "/static/media/alap2.4b35fcc6.jpg"
+  },
+  {
+    "revision": "c030deba95627275caa618f001d4c003",
+    "url": "/static/media/alap21.c030deba.jpg"
+  },
+  {
+    "revision": "1f2fffb7163c3ef6ed7cd677acb22397",
+    "url": "/static/media/alap3.1f2fffb7.jpg"
+  },
+  {
+    "revision": "d16aad238d511ffa58c22741770c8326",
+    "url": "/static/media/alap31.d16aad23.jpg"
+  },
+  {
+    "revision": "17c56bdbf612d45d2708829a6dbb73cf",
+    "url": "/static/media/alap4.17c56bdb.jpg"
+  },
+  {
+    "revision": "c8e5df4144b179dc7abc12e336ef402f",
+    "url": "/static/media/alap41.c8e5df41.jpg"
+  },
+  {
+    "revision": "ddbf81950ed001fc5a1e65edbf3c873e",
+    "url": "/static/media/alap5.ddbf8195.jpg"
+  },
+  {
+    "revision": "284deab6746238b80ea416ac90bde896",
+    "url": "/static/media/alap51.284deab6.jpg"
+  },
+  {
+    "revision": "85aae5d9ccae5bc4d1099750ef319fe9",
+    "url": "/static/media/alap6.85aae5d9.jpg"
+  },
+  {
+    "revision": "6fed27d02f12a9dd00c02de99b74ae8d",
+    "url": "/static/media/alap61.6fed27d0.jpg"
+  },
+  {
+    "revision": "edf0c07eeb349eed943905a954fd5a99",
+    "url": "/static/media/alap7.edf0c07e.jpg"
+  },
+  {
+    "revision": "c6197007d27c2500adf55689206deaa5",
+    "url": "/static/media/alap71.c6197007.jpg"
+  },
+  {
+    "revision": "d1b9c21950622125de2f350b5fae6f1e",
+    "url": "/static/media/alap8.d1b9c219.jpg"
+  },
+  {
+    "revision": "3fd4973c1eafa5c13dfd4c1891b2f857",
+    "url": "/static/media/buttonBack.3fd4973c.png"
+  },
+  {
+    "revision": "2acec85bd469fcb06f8bc85966a1bab5",
+    "url": "/static/media/buttonBelepes.2acec85b.png"
+  },
+  {
+    "revision": "d250d03183ea432c678b9cd3cbea319c",
+    "url": "/static/media/buttonFeladatok.d250d031.png"
+  },
+  {
+    "revision": "af8821c49da0ff2a537b66867d99eb76",
+    "url": "/static/media/buttonOk.af8821c4.png"
+  },
+  {
+    "revision": "344fd9a5753cf366b506fc524a01cf46",
+    "url": "/static/media/buttonOktatas.344fd9a5.png"
+  },
+  {
+    "revision": "c6d0fd8b027133b90afdfafb27043e55",
+    "url": "/static/media/buttonRolam.c6d0fd8b.png"
+  },
+  {
+    "revision": "9e3c8b7215832a7c47913a6b42b76070",
+    "url": "/static/media/buttonVizsga.9e3c8b72.png"
+  },
+  {
+    "revision": "50a342ad83f53de09d576613fd5ee1fa",
+    "url": "/static/media/iconFeladat.50a342ad.png"
+  }
+]);
